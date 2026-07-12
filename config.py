c40d71aea08b3a528c8f80db1d5770a17f6fae73
gunsnroses1000p-star <gunsnroses1000p@gmail.com>
2026-07-12 14:53:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+DEFAULT_IMAGE_MODEL = "black-forest-labs/FLUX.1-dev"
 
+DEFAULT_NEGATIVE = (
+    "blurry, low quality, cartoon, anime, CGI, 3d render, digital painting, "
+    "smooth plastic skin, perfect porcelain skin, doll skin, wax skin, "
+    "plastic skin, overprocessed face, beauty filter, glamour makeup, "
+    "thick eyebrows, blocky eyebrows, painted eyebrows, sharp eyebrows, "
+    "black lipstick, dark lipstick, heavy lipstick, oversized lips, "
+    "oversized eyes, distorted face, duplicate face, bad anatomy, "
+    "extra fingers, malformed hands, octane render, unreal engine, "
+    "subsurface scattering, ambient occlusion, perfectly symmetrical, "
+    "airbrushed"
+)
